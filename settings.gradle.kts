32a2c8337fd6a1a22583559fc7eb17fc2de96473
thegod <kamil29june@yandex.ru>
2025-05-18 12:03:16 +0300

    project init

---
+++ settings.gradle.kts
@@ -0,0 +1,4 @@
+
+
+rootProject.name="mcrnt_lnch_2"
+
---
+++ src/test/kotlin/ru/thegod/Mcrnt_lnch_2Test.kt
@@ -0,0 +1,19 @@
+package ru.thegod
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class Mcrnt_lnch_2Test {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
